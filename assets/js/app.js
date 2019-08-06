0fa18909fe999d15a4b823c08c3d9e76db6bb24d
Mathis Barré <mathis.barre@live.fr>
2019-08-06 16:28:09 +0200

    remove template loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,8 @@ var formElt = document.getElementById("form");
 var mainElt = document.getElementById("main");
 var inputElt = document.getElementById("searchBar");
 //var loaderElt = document.getElementsByClassName("lds-ring")[0];
-var data, templateDocument
+var data
+
 async function ajaxGet(url, type, callback) {
    const req = new XMLHttpRequest();
    req.open("GET", url);
@@ -26,55 +27,50 @@ async function ajaxGet(url, type, callback) {
 
 formElt.addEventListener("submit", onSubmit);
 
-function onSubmit (e = null) {
+function onSubmit(e = null) {
    //loaderElt.style.display = "block";
    if (e !== null) { e.preventDefault() };
-   ajaxGet("https://www.prevision-meteo.ch/services/json/" + inputElt.value , "application/json", onDataGet );
+   ajaxGet("https://www.prevision-meteo.ch/services/json/" + inputElt.value, "application/json", onDataGet);
 }
 
-function onDataGet (reponse) {
+function onDataGet(reponse) {
+   loaderElt.style.display = "none"
    data = JSON.parse(reponse);
    templateDocument = ajaxGet('/template.html', "text/html", onTemplateGet)
 };
 
-function onTemplateGet(reponse) {
-   //loaderElt.style.display = "none"
-   var output = ejs.render(reponse, {data: data});
-   
-   mainElt.innerHTML = output;
-};
-
 /********************* chart *************************/
+var dataTodayTemp = [12, 19, 3, 5, 2, 3]
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['1h00', '2h00', '3h00', '4h00','5h00','6h00','7h00','8h00','9h00','10h00','11h00','12h00'],
-        datasets: [{
-            label: 'Vitesse du vent',
-            data: [12, 19, 3, 5, 2, 3],
-            borderColor: [
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-        },
-        {
-         label: 'Température',
-         data: [25, 19, 56, 52, 90, 3,25, 19, 56, 52, 90, 3,25, 19, 56, 52, 90, 3],
+   type: 'line',
+   data: {
+      labels: ['1h00', '2h00', '3h00', '4h00', '5h00', '6h00', '7h00', '8h00', '9h00', '10h00', '11h00', '12h00'],
+      datasets: [{
+         label: 'Vitesse du vent',
+         data: dataTodayTemp,
          borderColor: [
-             'rgba(54, 162, 235, 1)'
+            'rgba(255, 99, 132, 1)'
          ],
          borderWidth: 1
-     }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+      },
+      {
+         label: 'Température',
+         data: [25, 19, 56, 52, 90, 3, 25, 19, 56, 52, 90, 3, 25, 19, 56, 52, 90, 3],
+         borderColor: [
+            'rgba(54, 162, 235, 1)'
+         ],
+         borderWidth: 1
+      }]
+   },
+   options: {
+      scales: {
+         yAxes: [{
+            ticks: {
+               beginAtZero: true
+            }
+         }]
+      }
+   }
 });
